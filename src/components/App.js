8ad4c3be1c6ad6b6f64c94091a58782d468f4538
Akash Patra <patraakash9@gmail.com>
2020-10-27 17:02:41 +0530

    Using the Custom Hook

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,22 @@ import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
+import useVideos from '../hooks/useVideos';
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
 
-  // setSelectedVideo(response.data.items[0]);
+  // Using Custom Hook and passing the defaultSearchTerm
+  const [videos, search] = useVideos('buildings');
+
+  // Whenever there is change in videos list, select the first video
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
   return (
     <div className="ui container">
-      <SearchBar onFormSubmit={onTermSubmit} />
+      <SearchBar onFormSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -1,27 +1,27 @@
-import {useState, useEffect} from React;
+import { useState, useEffect } from 'react';
 import youtube from '../apis/youtube';
 
 const useVideos = (defaultSearchTerm) => {
-    const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState([]);
 
-    useEffect(() => {
-        search(defaultSearchTerm);
-    }, []);
+  useEffect(() => {
+    search(defaultSearchTerm);
+  }, [defaultSearchTerm]);
 
-    const search = async (term) => {
-        // Add await keyword infront of API code
-        const response = await youtube.get('/search', {
-          params: {
-            q: term,
-          },
-        });
-    
-        // Change the state
-        setVideos(response.data.items);
-    };
+  const search = async (term) => {
+    // Add await keyword infront of API code
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+      },
+    });
 
-    // We can return array(React Convention) OR object(JS Convention)
-    return [videos, onTermSubmit];
+    // Change the state
+    setVideos(response.data.items);
+  };
+
+  // We can return array(React Convention) OR object(JS Convention)
+  return [videos, search];
 };
 
 export default useVideos;
